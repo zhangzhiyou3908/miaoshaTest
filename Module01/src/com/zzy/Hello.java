1fb4b221dd794cecef89e0b74248f75ed0eaace3
zhangzhiyou3908 <you17843103908@163.com>
2019-05-11 20:35:30 +0800

    Initial commit

---
+++ Module01/src/com/zzy/Hello.java
@@ -0,0 +1,19 @@
+package com.zzy;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+//这是单行注释
+public class Hello {
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        //在当前选中的文本中 shift+enter 到下一行   ctrl+ shift+enter  上一行
+        System.out.println("hello wrold");
+        Date date = new Date();
+
+        System.out.println(date);
+        //alt + shift+c  收起所有的方法 取消用 x
+        //查看某一个类的结构  ctrl +shift + u
+    }
+}
---
+++ javaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--    
+  Created by IntelliJ IDEA.
+  User: 26444
+  Date: 2019/5/11
+  Time: 19:16
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>测试</title>
+  </head>
+  <body>
+  你好
+  </body>
+</html>
